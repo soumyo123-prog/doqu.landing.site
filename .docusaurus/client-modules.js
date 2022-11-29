142951d9993b97cc13e836567d85f095acf270cb
Nurlanbek Duishoev <nurlanbek.duishoev@gmail.com>
2022-11-29 16:26:34 +0100

    added contact page, removed unused folder

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('D:\\classforma\\doqu.landing.site\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('D:\\classforma\\doqu.landing.site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('D:\\classforma\\doqu.landing.site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('D:\\classforma\\doqu.landing.site\\src\\css\\custom.css'),
-];
---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./index.css";
+
+import NavBar from "../components/NavBar";
+import MainSection from "../components/MainSection";
+import Features from "../components/Features";
+import UseCases from "../components/UseCases";
+import GettingStarted from "../components/GettingStarted";
+import Footer from "../components/Footer";
+
+export default function Contact() {
+  return (
+    <div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScFOHIJbQCYpA06fvuVgiCIjSnpthFSeNVWR1aTokWTk3Le4w/viewform?embedded=true" width="640" height="812" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </div>
+  );
+}
