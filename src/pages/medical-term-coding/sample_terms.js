d9007a17e7fdfff1b63fef96d5954fa9564c3bb4
Nurlanbek Duishoev <nurlanbek.duishoev@gmail.com>
2023-07-28 13:17:12 +0200

    bf

---
+++ src/pages/medical-term-coding/sample_terms.js
@@ -0,0 +1,4 @@
+const sample_terms = {
+    'meddra': ['sample meddra term 1', 'sample meddra term 2', 'sample meddra term 3'],
+    'icd-10-cm': ['sample icd-10-cm term 1', 'sample icd-10-cm term 2', 'sample icd-10-cm term 3'],
+}
