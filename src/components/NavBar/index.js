c2bfd42f34d49a555da1f6f612cd5c508d57a402
Soumyajyoti Dey <deysoumyajyoti2017@gmail.com>
2023-08-22 01:25:39 +0530

    navbar changes - 1

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import logo from "./favicon.ico";
 import "./index.css";
+import { FaAngleDown } from "react-icons/fa";
 
 const index = ({ activeTab }) => {
   const {
@@ -18,36 +19,75 @@ const index = ({ activeTab }) => {
       tab: "document-extraction",
     },
   ];
+
+  const dropdownData = [
+    {
+      title: "medical-term-coding",
+      submenu: ["Payment 1", "Payment 2", "Payment 3"],
+    },
+    {
+      title: "document-extraction",
+      submenu: ["Billing 1", "Billing 2", "Billing 3"],
+    },
+  ];
+
+  const [openMenu, setOpenMenu] = useState(null);
+  const timeoutRef = useRef(null);
+
+  const handleLinkMouseEnter = (index) => {
+    clearTimeout(timeoutRef.current);
+    setOpenMenu(index);
+  };
+
+  const handleMenuMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setOpenMenu(null);
+    }, 50);
+  };
+
   return (
-    <div className="header flex justify-between items-center">
+    <div className="header flex items-center">
       <a className="py-3 px-4 flex gap-2 items-center" href="/">
         <img src={logo} alt="logo" className="header-logo" />
         <h3 className="logo-text">EinMind</h3>
       </a>
-      <div className="px-8 flex items-center">
+      <ul className="px-8 flex items-center">
         {tabs.map((tab, index) => (
-          <>
-            <span
-              className={
-                tab.tab === activeTab ? "active header-tab" : "header-tab"
-              }
-              key={index}
-              onClick={() => (window.location.href = `/${tab.tab}`)}
-            >
-              {tab.name}
-            </span>
-            <span className="px-1">|</span>
-          </>
+          <li
+            className="p-2 relative group"
+            onMouseEnter={() => handleLinkMouseEnter(index)}
+            onMouseLeave={handleMenuMouseLeave}
+          >
+            <button className="flex flex-row items-center hover:text-indigo-500">
+              <span className="mr-1">{tab.name}</span>
+              <FaAngleDown
+                className={`transition-transform ${
+                  openMenu === index ? "transform rotate-180" : ""
+                }`}
+              />
+              {openMenu === index && (
+                <ul className="absolute left-0 top-5 mt-5 space-y-2 bg-white text-gray-900 border border-solid border-blue-500">
+                  {dropdownData[index].submenu.map(
+                    (submenuItem, submenuItemIndex) => {
+                      return (
+                        <li key={submenuItemIndex} className="px-4 py-2">
+                          {submenuItem}
+                        </li>
+                      );
+                    }
+                  )}
+                </ul>
+              )}
+            </button>
+          </li>
         ))}
         <span
           className="header-tab"
-          onClick={() =>
-            (window.location.href = `https://tally.so/r/wvejQX`)
-          }
+          onClick={() => (window.location.href = `https://tally.so/r/wvejQX`)}
         >
           Get started
         </span>
-      </div>
+      </ul>
     </div>
   );
 };
